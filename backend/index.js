6e1780500186f33d75b9eb5beea8b0608980f90a
DUTHIL Thomas <tduthil@petibonum.pedago.ipb.fr>
2023-10-16 15:38:22 +0200

    Initialisation partie back et front

---
+++ backend/index.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import faker from 'faker';
+import Sequelize from 'sequelize';
+import { Server } from 'socket.io';
