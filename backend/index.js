ca70b9109c3b80b91cd3e3e94b64610c5284fbcb
Paul Mistou <paul@MacBook-Air-de-Paul.local>
2023-11-14 17:00:48 +0100

    Creation BDD

--- backend/index.js
+++ backend/index.js
@@ -13,9 +13,55 @@ app.use(cors());
 /* Partie Sequelizer */
 const sequelize = new Sequelize('sqlite:database-shows.db');
 
-sequelize.sync({
-    force:true,
+// Modèle de la base de données Users
+const Users = sequelize.define('user', {
+    userID: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+    email: { type: Sequelize.STRING, unique: true },
+    password: { type: Sequelize.STRING },
 });
+
+// Modèle de la base de données Films
+const Films = sequelize.define('film', {
+    filmID: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+// Modèle de la base de données UsersActions
+const UsersActions = sequelize.define('useraction', {
+    watched: { type: Sequelize.BOOLEAN },
+    liked: { type: Sequelize.BOOLEAN },
+    towatch: { type: Sequelize.BOOLEAN },
+    userID: { type: Sequelize.INTEGER },
+    filmID: { type: Sequelize.INTEGER },
+});
+
+// Films.id et User.id doivent être clés étrangères primaires dans UsersActions
+Users.hasMany(UsersActions, {
+    foreignKey: 'userID',   
+});
+UsersActions.belongsTo(Users, {
+    foreignKey: 'userID', 
+});
+// Relation entre Film et UsersActions (relation 1,n)
+Films.hasMany(UsersActions, {
+    foreignKey: 'filmID',    
+});
+UsersActions.belongsTo(Films, {
+    foreignKey: 'filmID', 
+});
+
+// Création de la base de données
+Users.sync({ force: true }).then(() => {
+    console.log('Table Users créée');
+
+    Users.create({
+        email: 'a.b@gmail.com',
+        password: '1234',
+    });
+});
+
+Films.sync({ force: true }).then(() => console.log('Table Films créée'));
+
+UsersActions.sync({ force: true }).then(() => console.log('Table UsersActions créée'));
 
 /* Routes Express */
 // Route /
