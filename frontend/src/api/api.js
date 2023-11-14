4f0d7f8f2ddedc0ad43cf40b08511626c4a61cad
DUTHIL Thomas <tduthil@khost.pedago.ipb.fr>
2023-11-14 17:34:38 +0100

    Appel API fonctionnel + carousel fonctionnel

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,3 +1,19 @@
-import axios from axios;
+import { TMDB_API_KEY } from '../../env.js';
+const TMDB_URL = 'https://api.themoviedb.org/3/'
 
-const TMDB_URL = 'https://api.themoviedb.org/3/authentication/';
+export const getPopularMovie = async () => {
+    try {
+        let response = await fetch(TMDB_URL+"/movie/popular?language=fr-FR&page=1", {
+            method: 'GET',
+            headers: {
+              accept: 'application/json',
+              Authorization: 'Bearer '+TMDB_API_KEY
+            }
+          })
+        .then(response => response.json())
+        return response;
+
+    } catch (error) {
+        console.log(error);
+    }
+};
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,4 +1,27 @@
 import { createStore } from 'vuex';
+import { getPopularMovie } from '../api/api';
 
 export const store = createStore({
+    state: {
+        popularMovies: [],
+    },
+
+    getters: {
+        popularMovies: state => {
+            return state.popularMovies;
+        }
+    },
+
+    mutations: {
+        getPopularMovies (state, popularMovies) {
+            state.popularMovies = popularMovies
+        }
+    },
+
+    actions: {
+        async loadPopularMovies ({ commit }) {
+            const response = await getPopularMovie();
+            commit('getPopularMovies', response.results)
+        }
+    }
 });
